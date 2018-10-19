b86197d2da665592ead967ab79752bda1678a0d6
Student <pasha.fetisov.92@mail.ru>
2018-10-19 15:44:22 +0300

    Tri

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-incude <math>
-int foo (float m, float t, float x ) {
-    return (1/sqrt(2 * 3.14)) * exp(-pow(x - m), 2) / pow(t, 2);
+float foo(float a) {
+ return
+ if (a == ',');
+ std::cout << "Дробное число";<<std::endl
+ else std::cout << "Недробное число";<<std::endl
 }
 int main() {
-    float m=2, t=3, x=4
-a=foo();
+ float a=
+float b=foo();
 
 }
